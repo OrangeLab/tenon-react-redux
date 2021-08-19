c8b48aeff47067a9b93ad06e44be0faadba1ae10
kongdong <1216181067@qq.com>
2021-08-19 14:32:24 +0800

    适配tenon-react

--- example/Counter/index.js
+++ example/Counter/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { useSelector, useDispatch } from '../../src'
 import { decrement, increment } from '../features'
-import "./index.less"
 
 function Counter() {
   const count = useSelector((state) => state.counter.value);
@@ -13,12 +12,30 @@ function Counter() {
     dispatch(decrement())
   }
   return (
-    <view className="counter__box">
-      <text onTap={handleMinusTap} className="counter__button">点击-1</text>
-      <text>计数器：{count}</text>
-      <text onTap={handleAddTap} className="counter__button">点击+1</text>
+    <view style={counterBox}>
+      <text onTap={handleMinusTap} style={counterButton}>点击-1</text>
+      <text style={textStyle}>计数器：{count}</text>
+      <text onTap={handleAddTap} style={counterButton}>点击+1</text>
     </view>
   )
 }
-
+const counterBox = {
+  display: 'flex',
+  flexDirection: 'row',
+  alignItems: 'center'
+};
+const textStyle ={
+  fontSize: '40px',
+  fontWeight: 'bold',
+  marginLeft: '20px',
+  marginRight: '20px',
+};
+const counterButton = {
+  padding: '30px',
+  textAlign: 'center',
+  color: '#ffffff',
+  fontSize: '40px',
+  borderRadius: '10px',
+  backgroundColor: '#ff5500'
+};
 export default Counter;
